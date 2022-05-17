a7e067b29ce358f08cd75d41788d0d30c05aaff4
PiotrZettt <70430842+PiotrZettt@users.noreply.github.com>
2022-05-17 19:50:26 +0100

    Update settings.py
    updated settings.py

--- obrazy/settings.py
+++ obrazy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jrw2%s58e&4(sb@(5w00$l9_c-+=$jly+^z32==5&(*h5gj2kg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
